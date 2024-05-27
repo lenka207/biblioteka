2114e28b271f36e22f65799cf8c5273762eec32e
lenka207 <lenkapetkovic7@gmail.com>
2024-05-27 14:17:05 +0200

    Add project files.

---
+++ biblioteka/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace biblioteka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.username1 = new System.Windows.Forms.TextBox();
+            this.password1 = new System.Windows.Forms.TextBox();
+            this.potvrdi = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.izlaz = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username1
+            // 
+            this.username1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.username1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.username1.Location = new System.Drawing.Point(86, 369);
+            this.username1.Name = "username1";
+            this.username1.Size = new System.Drawing.Size(113, 22);
+            this.username1.TabIndex = 0;
+            this.username1.TextChanged += new System.EventHandler(this.username2);
+            // 
+            // password1
+            // 
+            this.password1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.password1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.password1.Location = new System.Drawing.Point(86, 397);
+            this.password1.Name = "password1";
+            this.password1.Size = new System.Drawing.Size(113, 22);
+            this.password1.TabIndex = 1;
+            this.password1.TextChanged += new System.EventHandler(this.password_TextChanged);
+            // 
+            // potvrdi
+            // 
+            this.potvrdi.Location = new System.Drawing.Point(112, 425);
+            this.potvrdi.Name = "potvrdi";
+            this.potvrdi.Size = new System.Drawing.Size(60, 26);
+            this.potvrdi.TabIndex = 2;
+            this.potvrdi.Text = "potvrdi";
+            this.potvrdi.UseVisualStyleBackColor = true;
+            this.potvrdi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(56, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(70, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(538, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(58, 36);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(828, 128);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(521, 245);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(828, 245);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(682, 326);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(828, 317);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // izlaz
+            // 
+            this.izlaz.BackColor = System.Drawing.Color.RosyBrown;
+            this.izlaz.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.izlaz.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.izlaz.Location = new System.Drawing.Point(507, 445);
+            this.izlaz.Name = "izlaz";
+            this.izlaz.Size = new System.Drawing.Size(49, 23);
+            this.izlaz.TabIndex = 11;
+            this.izlaz.Text = "izlaz";
+            this.izlaz.UseVisualStyleBackColor = false;
+            this.izlaz.Click += new System.EventHandler(this.izlaz_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(982, 538);
+            this.Controls.Add(this.izlaz);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.potvrdi);
+            this.Controls.Add(this.password1);
+            this.Controls.Add(this.username1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox username1;
+        private System.Windows.Forms.TextBox password1;
+        private System.Windows.Forms.Button potvrdi;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button izlaz;
+    }
+}
+
---
+++ biblioteka/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace biblioteka
+{
+    public partial class Form1 : Form
+    {
+        public string sifra = "123";
+        public string ime = "ja";
+        public bool administrator = false;
+        public Form1()
+        {
+            InitializeComponent();
+            SetCustomCursor();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+        private void SetCustomCursor1()
+        {
+            /*
+            // Učitaj sliku kursora iz resursa
+            Cursor customCursor;
+            using (var stream = new System.IO.MemoryStream(Properties.Resources.sl6))
+            {
+                customCursor = new Cursor(stream);
+            }
+
+            // Postavi kursor za celu formu
+            this.Cursor = customCursor;
+            */
+        }
+        private void SetCustomCursor()
+        {
+            try
+            {
+                // Učitaj sliku kursora iz resursa
+                using (var stream = new MemoryStream(Properties.Resources.images))
+                {
+                    Cursor customCursor = new Cursor(stream);
+                    // Postavi kursor za celu formu
+                    this.Cursor = customCursor;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to set custom cursor: " + ex.Message);
+            }
+        }
+        private void username2(object sender, EventArgs e)
+        {
+            string username = username1.Text;
+        }
+            
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+            string password = this.password1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (username1.Text == ime)
+            {
+                if (password1.Text == sifra)
+                {
+                    administrator = true;
+                    MessageBox.Show("Tacno");
+                }
+            }
+            if (administrator == false)
+            {
+                MessageBox.Show("Netacno ime ili sifra");
+            }
+        }
+
+        private void izlaz_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
